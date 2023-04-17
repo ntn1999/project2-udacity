31d0c1087a1869898b07450564e1976c14b6382d
ntn1999 <nam14nt@gamil.com>
2023-04-18 01:29:47 +0700

    second update

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ const app: express.Application = express();
 const address: string = '0.0.0.0:3000';
 
 app.use(bodyParser.json());
-
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World!');
 });
